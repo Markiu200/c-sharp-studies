cd55de177c2df4a3af6e7d0b1ce17bc84f8797a6
markiu200 <markiu2007@interia.pl>
2024-06-23 02:16:01 +0200

    dodaj testowy branch W celu przetestowania "git push <remote> <g1>:<g2>.

--- intermediate/Program.cs
+++ intermediate/Program.cs
@@ -6,7 +6,8 @@ namespace intermediate
     {
         static void Main(string[] args)
         {
-            intermediate.Study01.Study01.Run();
+            // intermediate.Study01.Study01.Run();
+            Console.WriteLine("branch-test");
         }
     }
 }
