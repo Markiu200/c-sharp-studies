03d43dffe3ccc3a75726a9d44f71e587b6d1e73b
Markiu200 <57598637+Markiu200@users.noreply.github.com>
2024-06-24 14:17:16 +0200

    Update Program.cs on GitHub to test git fetch

--- intermediate/Program.cs
+++ intermediate/Program.cs
@@ -9,6 +9,7 @@ namespace intermediate
             // intermediate.Study01.Study01.Run();
             Console.WriteLine("branch-test");
             Console.WriteLine("dodatkowy test");
+            Console.WriteLine("text added on GitHub");
         }
     }
 }
