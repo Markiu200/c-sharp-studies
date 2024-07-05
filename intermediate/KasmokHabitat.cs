fd0f7ca2c3f879d32bf929a49df93e3973c18e0a
Markiu200 <markiu2007@interia.pl>
2024-07-06 01:58:11 +0200

    Add abstract class for habitat and two derived classes implementing it

---
+++ intermediate/KasmokHabitat.cs
@@ -0,0 +1,26 @@
+﻿namespace intermediate
+{
+    internal abstract class KasmokHabitat  // if any member of a class is abstract, class must be abstract
+    {
+        /// <summary>
+        /// Abstract method cannot be implemented here, but must be implemented by derived class.
+        /// </summary>
+        public abstract string DescribeHabitat();
+    }
+
+    internal class ForestKasmokHabitat : KasmokHabitat
+    {
+        public override string DescribeHabitat()  // must be "override", otherwise compiler won't let it be
+        {
+            return "Big brown mushroom";
+        }
+    }
+
+    internal class WhiteKasmokHabitat : KasmokHabitat
+    {
+        public override string DescribeHabitat()  // must be "override", otherwise compiler won't let it be
+        {
+            return "Cave in snowy mountains";
+        }
+    }
+}
