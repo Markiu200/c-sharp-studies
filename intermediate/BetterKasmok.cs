83bd104e0d8f7eeb612dcece3aefb9633fc43909
Markiu200 <markiu2007@interia.pl>
2024-07-05 15:47:24 +0200

    Add BetterKasmok and ForestKasmok class for inheritance and composition examples

---
+++ intermediate/BetterKasmok.cs
@@ -0,0 +1,58 @@
+﻿namespace intermediate
+{
+    internal class BetterKasmok
+    {
+        /*
+         *  Properties
+         */
+        // auto-implemented
+        public string Name { get; set; }
+
+        // manual
+        DateTime _birthdate;
+        public DateTime Birthdate { 
+            get { return _birthdate; } 
+            private set
+            {
+                if (value > DateTime.Today)
+                    throw new ArgumentException("bithdate", "Birthdate cannot be from the future.");
+                this._birthdate = value;
+            }
+        }
+        public int Age
+        {
+            get { return (DateTime.Today - Birthdate).Days / 365; }
+        }
+
+        /* 
+         *  Constructors
+         */
+        public BetterKasmok()
+        {
+            this.Birthdate = DateTime.Today;
+            this.Name = "";
+        }
+
+        public BetterKasmok(string name, DateTime birthdate)
+        {
+            Name = name;
+            Birthdate = birthdate;
+        }
+
+        /*
+         *  Overrides
+         */
+        public override string ToString()
+        {
+            return $"kasmok {this.Name}, age {this.Age}";
+        }
+
+        /*
+         *  Methods
+         */
+        public void FixBirthdate(DateTime birthdate)
+        {
+            this.Birthdate = birthdate;
+        }
+    }
+}
---
+++ intermediate/ForestKasmok.cs
@@ -0,0 +1,20 @@
+﻿namespace intermediate
+{
+    internal class ForestKasmok : BetterKasmok
+    {
+        public ForestKasmok() : base()
+        {
+
+        }
+
+        public ForestKasmok(string name, DateTime birthdate) : base(name, birthdate)
+        {
+
+        }
+
+        public void HideUnderTheShroom()
+        {
+            Console.WriteLine($"{this.Name} quickly hid under the shroom");
+        }
+    }
+}
