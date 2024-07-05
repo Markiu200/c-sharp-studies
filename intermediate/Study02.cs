f9ce8a7f21337cdef4cb2075617a276706b77c18
Markiu200 <markiu2007@interia.pl>
2024-07-05 18:05:30 +0200

    Add upcasting example (explain it more)

--- intermediate/Study02.cs
+++ intermediate/Study02.cs
@@ -1,4 +1,5 @@
 ﻿using NspKprint;
+using System.Collections;
 
 namespace intermediate.Study02
 {
@@ -9,6 +10,7 @@ namespace intermediate.Study02
             // Create example list for kasmoks downcasting and upcasting. 
             List<BetterKasmok> betterKasmokList = new List<BetterKasmok>();
             betterKasmokList.Add(new BetterKasmok("Kasmok", new DateTime(1992, 12, 21)));
+            // This is possible, because ForestKasmok can be (and will be) upcasted to BetterKasmok.
             betterKasmokList.Add(new ForestKasmok("Piskocz", new DateTime(2000, 4, 28)));
 
             // Downcasting example.
@@ -19,7 +21,7 @@ namespace intermediate.Study02
                 // "is" keyword can be used to compare if given class is another class.
                 if (kasmok is ForestKasmok)
                 {
-                    Console.Write(". That's a Forest Kasmok!");
+                    Console.Write(". That's a forest kasmok!");
                     // Downcasting BetterKasmok to Forestkasmok (because child class is "under" parent, I guess?)
                     // "as" keyword will make sure if this casting is valid (instead of)
                     // ForestKasmok fkasmok = (ForestKasmok)kasmok
@@ -41,6 +43,36 @@ namespace intermediate.Study02
             // 
             // Unhandled exception. System.ArgumentException: bithdate (Parameter 'Birthdate cannot be from the future.')
             // betterKasmokList.Find(x => x.Name == "Piskocz").FixBirthdate(new DateTime(2050, 2, 2));
+
+            // Downcasting example refactored. 
+            Kprint.Title("Downcasting example (refactored):");
+            foreach (var kasmok in betterKasmokList)
+            {
+                Console.Write(kasmok);
+                ForestKasmok? fkasmok = kasmok as ForestKasmok;
+                if (fkasmok is not null)
+                {
+                    Console.Write(". That's a forest kasmok! ");
+                    fkasmok.HideUnderTheShroom();
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            // Upcasting example. 
+            Kprint.Title("Upcasting example:");
+            // We know for sure that 
+            BetterKasmok upcastedKasmok = betterKasmokList[1];
+            Console.WriteLine("betterKasmokList[1] is {0}, however it cannot hide under the shroom due to being BetterKasmok at the moment.", upcastedKasmok.Name);
+            // Error	CS1061	'BetterKasmok' does not contain a definition for 'HideUnderTheShroom' and no accessible extension method 'HideUnderTheShroom'
+            // accepting a first argument of type 'BetterKasmok' could be found (are you missing a using directive or an assembly reference?)
+            // upcastedKasmok.HideUnderTheShroom();
+            //
+            // Even thougn "upcastedKasmok" and "betterKasmokList[1] are referencing the same object,
+            // the _view_ of upcastedKasmok is limited to what BetterKasmok can comprehend. 
+
         }
     }
 }
