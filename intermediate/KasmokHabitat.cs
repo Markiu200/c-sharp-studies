0648066b65edefa9f6c0e0b0fd52524c4b16e55e
Markiu200 <markiu2007@interia.pl>
2024-07-06 02:56:15 +0200

    Add comment about "sealed" keyword. No example introduced

--- intermediate/KasmokHabitat.cs
+++ intermediate/KasmokHabitat.cs
@@ -26,6 +26,13 @@
 
     internal class NotAKasmokHabitat : WhiteKasmokHabitat
     {
+        // Instead of overriding methods or classes, you can prevent this from happening, that is:
+        // preventing from creating derivatives of a class
+        // preventing from overriding a method
+        // Simply use "sealed" keyword in method or class declaration.
+        // MSDN says that it makes better performance, but it's a slight upgrade and probably isn't worth it, and may be in your way in the future. 
+        // No need to use it unless making derivatives is indeed undesired.
+        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/sealed
         public override string DescribeHabitat()
         {
             return "Turns out you can override overriden thing";
