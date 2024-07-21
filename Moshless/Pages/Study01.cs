db474b8ca2ef092af516416eadd8cf10f7bc9cde
Markiu200 <markiu2007@interia.pl>
2024-07-21 13:50:06 +0200

    feat: add test code snippet to see if classes from Intermediate work

---
+++ Moshless/Pages/Study01.cs
@@ -0,0 +1,14 @@
+﻿using intermediate;
+using NspKprint;
+
+namespace Moshless.Pages
+{
+    internal static class Study01
+    {
+        public static void Run()
+        {
+            BetterKasmok kasmok = new BetterKasmok("Kasmok", new DateTime(1992, 12, 12));
+            Console.WriteLine($"Kasmok's name: {kasmok.Name}");
+        }
+    }
+}
