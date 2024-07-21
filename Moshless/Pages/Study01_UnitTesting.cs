4888aaf59d8cf7162bcbd6b58d4c98b41df072a1
Markiu200 <markiu2007@interia.pl>
2024-07-21 18:23:33 +0200

    example(playable): try CamelCaser on a string

---
+++ Moshless/Pages/Study01_UnitTesting.cs
@@ -0,0 +1,13 @@
+﻿using intermediate;
+using NspKprint;
+
+namespace Moshless.Pages
+{
+    internal static class Study01_UnitTesting
+    {
+        public static void Run()
+        {
+            Console.WriteLine("to ten kasmok".CamelCase());
+        }
+    }
+}
