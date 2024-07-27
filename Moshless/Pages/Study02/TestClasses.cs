35813c0f910c98ce62a33d09107db27bcd9e1318
Markiu200 <markiu2007@interia.pl>
2024-07-27 15:43:04 +0200

    new: add Study02.TestClasses
    For experimenting with class / struct differences

---
+++ Moshless/Pages/Study02/TestClasses.cs
@@ -0,0 +1,32 @@
+﻿namespace Moshless.Pages.Study02
+{
+    public class TestClass
+    {
+        public string text = "";
+        public int number = 0;
+    }
+    public struct TestStruct
+    {
+        // A 'struct' with field initializers must include an explicitly declared constructor.
+        //public string text = "";
+        //public int number = 0;
+        public string text;
+        public int number;
+    }
+    public struct ValueStruct
+    {
+        public int value;
+    }
+
+    public static class TestClasses
+    {
+        public static void ClassChangeName(TestClass test)
+        {
+            test.text = "changed";
+        }
+        public static void StructChangeName(TestStruct test)
+        {
+            test.text = "changed";
+        }
+    }
+}
